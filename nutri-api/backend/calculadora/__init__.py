749899974f62fe6504787e7a0eabd59ddf2c52a1
Giovani Stuchi <24.00645-9>
2026-03-24 12:20:36 -0300

    remove nutri-api (projeto separado)

--- nutri-api/backend/calculadora/__init__.py
+++
@@ -1,5 +0,0 @@
-from .tmb      import calcular_tmb, calcular_tdee
-from .imc      import calcular_imc
-from .macros   import calcular_macros
-from .cardapio import gerar_cardapio
-from .insights import gerar_dica, gerar_insight
